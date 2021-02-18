c9fb7d44766dcb648bcb9281a941abd36d9b2609
Alex Kanitz <alexander.kanitz@unibas.ch>
2021-02-18 17:45:27 +0100

    build: initial configuration

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+from code import __version__
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Read requirements from file
+install_requires = []
+req = root_dir + '/requirements.txt'
+if os.path.isfile(req):
+    with open(req) as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name="test",
+    version=__version__,
+    description="Sandbox for testing semantic releases"
+    url="https://github.com/uniqueg/semantic-releases",
+    author="ELIXIR Cloud & AAI",
+    author_email="alexander.kanitz@alumni.ethz.ch",
+    maintainer="Alexander Kanitz",
+    maintainer_email="alexander.kanitz@alumnni.ethz.ch",
+    packages=find_packages(),
+    install_requires=install_requires,
+    include_package_data=True,
+    setup_requires=[
+        "setuptools_git == 1.2",
+    ],
+)
